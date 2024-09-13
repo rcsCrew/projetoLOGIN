d573668e32d070bc56f4987ab939ec87b55b96fe
ramon senger <51542841+rcsCrew@users.noreply.github.com>
2024-09-13 15:28:56 -0300

    funcionando

---
+++ loginv2/loginv2/src/app/pages/registrar/registrar.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registrar',
+  templateUrl: './registrar.component.html',
+  styleUrls: ['./registrar.component.css']
+})
+export class RegistrarComponent {
+  registrarData = {
+    username: '',
+    password: '',
+    email: ''  // Incluindo um campo de email para o registro
+  };
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    const headers = { 'Content-Type': 'application/json' };
+
+    // Enviando os dados para o endpoint que lida com o registro de novos usuários
+    this.http.post<any>('https://ramonsenger.com/testes/registrar.php', this.registrarData, { headers })
+      .subscribe(response => {
+        console.log('Response:', response);
+
+        if (response.status === 'success') {
+          alert('Usuário registrado com sucesso!');
+
+          // Opcionalmente, redireciona o usuário para a página de login ou dashboard
+          this.router.navigate(['/login']);
+        } else {
+          alert(response.message);
+        }
+      }, error => {
+        console.log('Erro: ', error);
+        alert('Erro ao tentar registrar. Tente novamente mais tarde.');
+      });
+  }
+  navigateToLogin(){
+    this.router.navigate(['/login']);
+  }
+}
